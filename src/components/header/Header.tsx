0a61f3b5c8256da48aff65f63dab38e9397c3c2d
Imane El Boutachfaiti <imane.elboutachfaiti@gmail.com>
2021-01-26 00:20:25 +0100

    create Header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import './Header.css';
+export const Header = () => {
+  return (
+    <header className="header-section">
+      <span className="logo">Fashion</span>
+      <nav className="header-menu">
+        <a className="menu-item">Home</a>
+        <a className="menu-item">Recipes</a>
+        <a className="menu-item">Article</a>
+        <a className="menu-item">Contact</a>
+        <a className="menu-item">Purchase</a>
+      </nav>
+    </header>
+  );
+};
