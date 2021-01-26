876726518e043b8164225b0b6b3e1425811a53b9
Imane El Boutachfaiti <imane.elboutachfaiti@gmail.com>
2021-01-26 20:07:22 +0100

    add main content component + fix logo text

--- src/components/main-content/Main.tsx
+++ src/components/main-content/Main.tsx
@@ -3,9 +3,20 @@ import "./Main.css";
 
 export const Main = () => {
   return (
-    <main>
+    <main className="main-content-section">
       <div>
         <img className="content-image" src={contentImage} alt="content-image" />
+      </div>
+      <div className="main-content">
+        <span className="image-text">Vehicle</span>
+        <h1 className="main-title">
+          One of Saturn’s largest rings may be newer than anyone
+        </h1>
+        <div className="main-text image-text">
+          <span>June 6, 2019</span>
+          <span>By Rickie Baroch</span>
+          <span>4 comments</span>
+        </div>
       </div>
     </main>
   );
